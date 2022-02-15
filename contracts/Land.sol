fb3805814a41a85b2350d79353216da87f734fe9
unknown <haris.nabeel@ideofuzion.com>
2022-02-15 18:33:29 +0500

    Token Implemented

---
+++ contracts/Land.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "hardhat/console.sol";
+contract Land is ERC721{
+
+    address _owner;
+    constructor() ERC721("My Land","ML"){
+        console.log("Hey! this is Haris Nabeel");
+        console.log("This contract is deployed by " , msg.sender);
+        _owner = msg.sender;
+    }
+
+        using Counters for Counters.Counter;
+        Counters.Counter myCounter;
+        // event MyLog(uint256);
+        function _mint() public payable{
+            require(msg.value == 0.000000000000000001 ether , "Send more ethers: 1 ether is required");
+            require(balanceOf(msg.sender)==0, "You can not have more than 1 Land");
+            myCounter.increment();
+            console.log(myCounter.current() );
+            // emit MyLog(myCounter.current());
+            _safeMint(msg.sender,myCounter.current());
+        }
+
+        function withdraw() public onlyOwner(msg.sender){
+            payable(msg.sender).transfer(address(this).balance);
+        }
+
+        modifier onlyOwner(address a) {
+            require(a==_owner,"Caller is not the owner");
+            _;
+        }
+        
+}
---
+++ scripts/run.js
@@ -0,0 +1,31 @@
+const main = async () => {
+  // const Land = await hre.ethers.getContractFactory("Land");
+  // const LandContract = await Land.deploy();
+  // await LandContract.deployed();
+  // console.log("Contract deployed to:", LandContract.address);
+  const [randomPerson1, randomPerson2] = await hre.ethers.getSigners();
+  console.log("HAHAHAHA");
+  console.log(randomPerson1.address);
+  console.log("HAHAHAHA 222");
+  let createNft = await LandContract._mint({ value:1});
+  await createNft.wait();
+   createNft = await LandContract.connect(randomPerson2)._mint({value:1});
+  await createNft.wait();
+  //  createNft = await LandContract._mint({from:randomPerson2.address, value:1});
+  // await createNft.wait();
+  // console.log("Land created for" + randomPerson2);
+
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0); // exit Node process without error
+  } catch (error) {
+    console.log(error);
+    process.exit(1); // exit Node process while indicating 'Uncaught Fatal Exception' error
+  }
+  // Read more about Node exit ('process.exit(num)') status codes here: https://stackoverflow.com/a/47163396/7974948
+};
+
+runMain();
